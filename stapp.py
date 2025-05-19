221b9c7a31620fb8e6164f195c72808da84f0c0b
Oncejae <imop1289@naver.com>
2025-05-19 14:42:54 +0900

    componentsstapp.py

---
+++ stapp.py
@@ -0,0 +1,22 @@
+#pip install streamlit
+import streamlit as st
+
+x = st.slider('Select a value')
+st.write(x, 'squared is', x ** x)
+st.title("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'"a+a r^1+a r^2+a r^3"')
+st.image("kid.jpg")
+st.audio("Audio.mp3")
+st.video("video.mp4")
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick ypur gender',['Male','Female'])
+st.selectbox('Pick ypur gender',['Male','Female'])
+st.multiselect('choose a planet',['Jupiter','Mars', 'Neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent']) 
+st.slider('Pick a number', 0.50)                                 
